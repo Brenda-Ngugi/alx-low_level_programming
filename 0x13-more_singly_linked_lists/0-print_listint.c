6b58e1fa9754792bbeff1d981efe2b06f9f5615d
brea <brendangugi7@gmail.com>
2023-05-01 12:02:55 -0700

    0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,12 @@
  */
 size_t print_listint(const listint_t *h);
 {
-	const listint_t *current = h;
+	size_t num = 0;
 
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
+		num++;
 		current = current->next;
 	}
 	return (num);
