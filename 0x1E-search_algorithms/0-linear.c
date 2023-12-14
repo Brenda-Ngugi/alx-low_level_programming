3678733208328356ad7869a85b465f5355ff9022
Brenda Ngugi <brendangugi7@gmail.com>
2023-12-14 09:18:19 -0800

    search_alogrithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ *linear_search -  for a value in an array using linear search
+ *@array: emacs pointer to the first element of the array to search in
+ *@size: number of elements in array
+ *@value: value to search for
+ *Return: First occurence of value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (size == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
